9896505dbacd76ce11d3b8f35179d944d4a8bc15
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-04-26 13:34:27 +0300

    Added basic program code for processing command line arguments

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 const (
 	GetPort  = "need to port"
 	SendPort = "need port"
 )
 
 func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("go 0")
+		return
+	}
 
 }
