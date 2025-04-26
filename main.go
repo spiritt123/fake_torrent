df6af0cc7055a10e503dbdba993c20bc31ea7681
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-04-26 13:53:32 +0300

    add main function to start server and check file

--- main.go
+++ main.go
@@ -3,17 +3,37 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 const (
-	GetPort  = "need to port"
-	SendPort = "need port"
+	cmdGetPort = "GET_PORT"
+	SendPort   = "SEND_PORT"
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("go 0")
+	if len(os.Args) < 2 {
+		fmt.Println("client <портПрослушивания> [файлИлиАдресПира]")
 		return
 	}
 
+	listenPort := os.Args[1]
+	var peerAddress string
+
+	if len(os.Args) >= 3 {
+		peerArg := os.Args[2]
+		if _, err := os.Stat(peerArg); err == nil {
+			content, err := os.ReadFile(peerArg)
+			if err != nil {
+				fmt.Printf("Ошибка чтения файла %s: %v\n", peerArg, err)
+				return
+			}
+			peerAddress = strings.TrimSpace(string(content))
+		} else {
+			peerAddress = peerArg
+		}
+	}
+	// TODO Add func relize startServer StartInteractive
+	go startServer(listenPort)
+	startInteractiveMode(listenPort, peerAddress)
 }
